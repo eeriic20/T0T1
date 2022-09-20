29e6235430606e3552d44911bd84ecd67e3b3d26
PORTATIL-ERIC\ericq <ericquintero2002@gmail.com>
2022-09-20 11:48:58 +0200

    Subiendo fichero .php

---
+++ MobApp/sayingModel.php
@@ -0,0 +1,18 @@
+<?php
+
+class sayingModel {
+    function getSaying($n){
+        $sayings =[
+            "A quien madruga, Dios le ayuda",
+            "No hay mal que por bien no venga",
+            "De tal palo, tal astilla",
+            "En casa del herrero cuchara de palo",
+            "El que no corre, vuela",
+            "A lo hecho, pecho",
+            "Ojo por ojo, diente por diente",
+            "A rey muerto, rey puesto"
+        ];
+        return $sayings[$n];
+    }
+}
+?>
